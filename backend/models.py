814467e3d173b6cf4fec156a88a7dd42ba3f9e92
Mark <mark2004kyki@gmail.com>
2025-03-02 21:27:35 +0100

    last changes

--- backend/models.py
+++ backend/models.py
@@ -75,7 +75,7 @@ db_params = {
     "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),  # 👈 Use `postgres`, NOT `localhost`
+    "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
 }
 
